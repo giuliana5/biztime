bf02d7c0811db2ef07788cd27aa75a9c04d1a482
giuliana <giuliana.torres05@gmail.com>
2023-06-21 18:25:19 -0600

    Added route to update existing company.

--- routes/companies.js
+++ routes/companies.js
@@ -23,6 +23,8 @@ router.get("/", async function (req, res, next) {
 
 router.get("/:code", async function (req, res, next) {
     try {
+        let code = req.params.code;
+
         const result = await db.query(
             `SELECT code, name, description
             FROM companies
@@ -55,6 +57,28 @@ router.post("/", async function (req, res, next) {
 
         return res.status(201).json({"company": result.rows[0]});
     } catch(err) {
+        return next(err);
+    }
+});
+
+router.put("/:code", async function (req, res, next) {
+    try {
+        let {name, description} = req.body;
+        let code = req.params.code;
+
+        const result = await db.query(
+            `UPDATE companies
+            SET name=$1, description=$2
+            WHERE code = $3
+            RETURNING code, name, description`, [name, description, code]
+        );
+
+        if (result.rows.length === 0) {
+            throw new ExpressError(`No such company: ${code}`, 404);
+        } else {
+            return res.json({"company": result.rows[0]});
+        }
+    } catch (err) {
         return next(err);
     }
 });
