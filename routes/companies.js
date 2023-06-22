59cde753891d628ba48fcc7db02442d026e2a677
giuliana <giuliana.torres05@gmail.com>
2023-06-21 18:53:13 -0600

    Added to company put route to include invoice data.

--- routes/companies.js
+++ routes/companies.js
@@ -25,17 +25,27 @@ router.get("/:code", async function (req, res, next) {
     try {
         let code = req.params.code;
 
-        const result = await db.query(
+        const compResult = await db.query(
             `SELECT code, name, description
             FROM companies
             WHERE code = $1`, [code]
         );
 
-        if (result.rows.length === 0) {
+        const invResult = await db.query(
+            `SELECT id 
+            FROM invoices
+            WHERE comp_code = $1`, [code]
+        );
+
+
+        if (compResult.rows.length === 0) {
             throw new ExpressError(`Company not found: ${code}`, 404);
         }
 
-        const company = result.rows[0];
+        const company = compResult.rows[0];
+        const invoices = invResult.rows;
+
+        company.invoices = invoices.map(inv => inv.id);
 
         return res.json({"company": company});
 
