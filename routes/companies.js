98cba0eb5ab2c36d9e6c21d866ec67972d744c71
giuliana <giuliana.torres05@gmail.com>
2023-06-21 18:20:16 -0600

    Added post route to add a company.

--- routes/companies.js
+++ routes/companies.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const ExpressError = require("../expressError");
 const db = require("../db");
+const slugify = require("slugify");
 
 let router = new express.Router();
 
@@ -39,6 +40,23 @@ router.get("/:code", async function (req, res, next) {
     } catch(err) {
         return next(err);
     }
-})
+});
+
+router.post("/", async function (req, res, next) {
+    try {
+        let {name, description} = req.body;
+        let code = slugify(name, {lower: true});
+
+        const result = await db.query(
+            `INSERT INTO companies (code, name, description)
+            VALUES ($1, $2, $3)
+            RETURNING code, name, description` [code, name, description]
+        );
+
+        return res.status(201).json({"company": result.rows[0]});
+    } catch(err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
